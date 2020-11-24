2447fe631a5970dd74ddc11ab9ccce4641cc4a32
jidicula <>
2020-11-24 17:34:01 -0500

    [fix][Emacs] Remove cruft

--- init.el
+++ init.el
@@ -32,18 +32,14 @@ which is unsafe because it allows man-in-the-middle attacks.
 There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable
-  ;; as desired
+
+  ;; Use MELPA archive
   (add-to-list 'package-archives (cons
 				  "melpa"
 				  (concat proto "://melpa.org/packages/")
 				  )
 	       t)
-  ;; (add-to-list 'package-archives (cons
-  ;; 				  "melpa-stable"
-  ;; 				  (concat proto "://stable.melpa.org/packages/")
-  ;; 				  )
-  ;; 	       t)
+
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives
