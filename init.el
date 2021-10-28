57c3dd405f3de65ef90ac73eaf630d54812ac83b
jidicula <johanan@forcepush.tech>
2021-10-28 11:38:03 -0400

    feat(Emacs): Handle non-Darwin OS

--- init.el
+++ init.el
@@ -124,9 +124,12 @@ There are two things you can do about this warning:
 	 ("C-c g" . magit-file-dispatch))
   :config
   (if (string= (shell-command-to-string "arch") "arm64")
-    (setq magit-git-executable "/opt/homebrew/bin/git")
-    (setq magit-git-executable "/usr/local/bin/git")
-  )
+      (setq magit-git-executable "/opt/homebrew/bin/git")
+    (if (string= (shell-command-to-string "uname") "Darwin")
+        (setq magit-git-executable "/usr/local/bin/git")
+        (setq magit-git-executable "/usr/bin/git")
+        )
+    )
   
   (setq auth-sources '("~/.authinfo"))
   (setq magit-display-buffer-function
@@ -863,7 +866,7 @@ then enter the text in that file's own buffer.")
 ;; Fancy titlebar for macOS
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
-(setq ns-use-proxy-icon  nil)
+(setq ns-use-proxy-icon nil)
 
 ;; sgml
 (setq sgml-quick-keys 'indent)
