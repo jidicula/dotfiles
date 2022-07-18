993836b46c7d5457332ce4224d782048f83ac51b
jidicula <johanan@forcepush.tech>
2022-07-18 13:25:23 -0400

    feat(setup): Configure touchID sudo auth

--- setup.sh
+++ setup.sh
@@ -36,6 +36,25 @@ ln -sfv "$DOTFILESDIR/gitconfig" "$HOME/.gitconfig"
 ln -sfv "$DOTFILESDIR/gitignore" "$HOME/.gitignore"
 ln -sfv "$DOTFILESDIR/git-templates" "$HOME/.git-templates"
 
+if [[ -n "$OS" ]]; then
+	if ls /usr/lib/pam | grep -q "pam_tid.so"; then
+		echo "Configuring sudo authentication using TouchID:"
+		PAM_FILE="/etc/pam.d/sudo"
+		FIRST_LINE="# sudo: auth account password session"
+		if grep -q pam_tid.so "$PAM_FILE"; then
+			echo "OK"
+		elif ! head -n1 "$PAM_FILE" | grep -q "$FIRST_LINE"; then
+			echo "$PAM_FILE is not in the expected format!"
+		else
+			TOUCHID_LINE="auth       sufficient     pam_tid.so"
+			sudo sed -i .bak -e \
+				"s/$FIRST_LINE/$FIRST_LINE\n$TOUCHID_LINE/" \
+				"$PAM_FILE"
+			sudo rm "$PAM_FILE.bak"
+			echo "OK"
+		fi
+	fi
+fi
 
 if [[ -n "$OS" ]]; then
 
