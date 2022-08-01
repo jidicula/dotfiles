3fe710d0aeb5040a92a0f0caf6e727fb08643a9d
jidicula <git@johanan.dev>
2022-08-01 13:40:17 -0400

    ci: Clean and cache /Applications

--- actions-runner-preclean.sh
+++ actions-runner-preclean.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ $OSTYPE == darwin* ]]; then
-	rm '/usr/local/bin/aws' \
-		'/usr/local/bin/go' \
-		'/usr/local/bin/clusterdb' \
-		'/usr/local/bin/dotnet'
-
+	rm -rf /Applications && mkdir /Applications
+	rm "/usr/local/bin/aws*" \
+		"/usr/local/bin/go*" \
+		"/usr/local/bin/clusterdb" \
+		"/usr/local/bin/dotnet" \
+		"/usr/local/bin/createdb" \
+		"/usr/local/bin/anthoscli"
 fi
