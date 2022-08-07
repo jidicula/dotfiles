751c2a6913602cb13575408ef661d04ea7a911ab
jidicula <git@johanan.dev>
2022-08-07 18:12:12 -0400

    ci: Sudo remove pre-installed tools from runner

--- actions-runner-preclean.sh
+++ actions-runner-preclean.sh
@@ -3,7 +3,7 @@ shopt -s extglob
 
 if [[ $OSTYPE == darwin* ]]; then
 	sudo rm -rf /Applications/!(Safari.app|Xcode*)
-	rm "usr/local/bin/aws" \
+	sudo rm "usr/local/bin/aws" \
 		"usr/local/bin/go" \
 		"usr/local/bin/dotnet"
 fi
