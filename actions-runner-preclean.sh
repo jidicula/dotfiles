1c137ebb545b9bb6cc5ac69219dde9d0989e067e
jidicula <git@johanan.dev>
2022-08-07 15:50:10 -0400

    ci: Find where tools are installed on runner

--- actions-runner-preclean.sh
+++ actions-runner-preclean.sh
@@ -3,10 +3,11 @@ shopt -s extglob
 
 if [[ $OSTYPE == darwin* ]]; then
 	sudo rm -rf /Applications/!(Safari.app)
-	rm "/usr/local/bin/aws*" \
-		"/usr/local/bin/go*" \
-		"/usr/local/bin/clusterdb" \
-		"/usr/local/bin/dotnet" \
-		"/usr/local/bin/createdb" \
-		"/usr/local/bin/anthoscli"
+	# rm "$(
+	which "aws" \
+		"go" \
+		"clusterdb" \
+		"dotnet" \
+		"createdb"
+	# )"
 fi
