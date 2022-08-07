d5a37296f0ae023c737a72492c3e8f005972aa65
jidicula <git@johanan.dev>
2022-08-07 19:24:59 -0400

    ci: Unflip extglob after /Applications cleanup

--- actions-runner-preclean.sh
+++ actions-runner-preclean.sh
@@ -3,6 +3,7 @@ shopt -s extglob
 
 if [[ $OSTYPE == darwin* ]]; then
 	sudo rm -rf /Applications/!(Safari.app|Xcode*)
+	shopt -u extglob
 	rm "/usr/local/bin/aws*" \
 		"/usr/local/bin/go*" \
 		"/usr/local/bin/dotnet"
