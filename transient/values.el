3995088c64becb73ce02b03e6a3c36d62544ae72
jidicula <git@johanan.dev>
2022-09-11 14:42:02 -0400

    feat(emacs): Always annotate tags in Magit

--- transient/values.el
+++ transient/values.el
@@ -1,3 +1,4 @@
 ((magit-diff:magit-diff-mode "--no-ext-diff" "--stat" "--show-signature")
  (magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature")
+ (magit-tag "--annotate")
  (rg-menu "--hidden" "--glob='!.git'"))
