adfd811364f422936dbb9e4f7d354d818dc249a6
jidicula <git@johanan.dev>
2022-09-25 18:46:47 -0400

    fix(emacs): Suppress compilation warnings

--- init.el
+++ init.el
@@ -988,3 +988,15 @@ then enter the text in that file's own buffer.")
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
