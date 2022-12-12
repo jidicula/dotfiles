c6ae8858442e27080713360da20f642c81b0b033
jidicula <git@johanan.dev>
2022-12-12 14:02:46 -0500

    feat(emacs): magit fetch with prune

--- transient/values.el
+++ transient/values.el
@@ -1,4 +1,5 @@
 ((magit-diff:magit-diff-mode "--no-ext-diff" "--stat" "--show-signature")
+ (magit-fetch "--prune")
  (magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature")
  (magit-tag "--annotate")
  (rg-menu "--hidden" "--glob='!.git'"))
