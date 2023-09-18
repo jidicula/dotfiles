f72a3ee471577755ed2d87af46dd40b6e964965b
jidicula <git@johanan.dev>
2023-09-18 14:00:13 -0400

    feat(emacs): Don't set glob transient setting for rg

--- transient/values.el
+++ transient/values.el
@@ -3,4 +3,4 @@
  (magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature")
  (magit-pull "--rebase")
  (magit-tag "--annotate")
- (rg-menu "--hidden" "--glob='!.git'"))
+ (rg-menu "--hidden" "--pcre2"))
