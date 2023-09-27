d0a45d2b4771fb5d69d1f1a9a1d72ac1b2987a5e
jidicula <git@johanan.dev>
2023-09-27 11:13:38 -0400

    fix(macos): Support previous macOS versions with touchid-sudo.sh

--- script/touchid-sudo.sh
+++ script/touchid-sudo.sh
@@ -6,14 +6,31 @@ if ls /usr/lib/pam/pam_tid.so*; then
 	echo "Configuring sudo authentication using TouchID:"
 	PAM_LOCAL_FILE="/etc/pam.d/sudo_local"
 	TOUCHID_LINE="auth       sufficient     pam_tid.so"
-	sudo cp "$PAM_LOCAL_FILE.template" "$PAM_LOCAL_FILE"
-	if ! sed -n '3p' "$PAM_LOCAL_FILE" | grep -q "${TOUCHID_LINE}"; then
-		echo "$PAM_LOCAL_FILE is not in the expected format!"
+	# macOS versions before 14.0 Sonoma
+	if ! sudo cp "$PAM_LOCAL_FILE.template" "$PAM_LOCAL_FILE"; then
+		TOP_LEVEL_SUDO_FILE="/etc/pam.d/sudo"
+		FIRST_LINE="# sudo: auth account password session"
+		if grep -q pam_tid.so "$TOP_LEVEL_SUDO_FILE"; then
+			echo "Already configured! No-op exiting."
+		elif ! head -n1 "$TOP_LEVEL_SUDO_FILE" | grep -q "$FIRST_LINE"; then
+			echo "$TOP_LEVEL_SUDO_FILE is not in the expected format!"
+		else
+			TOUCHID_LINE="auth       sufficient     pam_tid.so"
+			sudo sed -i .bak -e \
+				"s/$FIRST_LINE/$FIRST_LINE\n$TOUCHID_LINE/" \
+				"$TOP_LEVEL_SUDO_FILE"
+			# sudo rm "$TOP_LEVEL_SUDO_FILE.bak"
+			echo "TouchID sudo configuration complete."
+		fi
 	else
-		sudo sed -i .bak -e \
-			"s/# $TOUCHID_LINE/$TOUCHID_LINE/" \
-			"$PAM_LOCAL_FILE"
-		sudo rm "$PAM_LOCAL_FILE.bak"
-		echo "TouchID sudo configuration complete."
+		if ! sed -n '3p' "$PAM_LOCAL_FILE" | grep -q "${TOUCHID_LINE}"; then
+			echo "$PAM_LOCAL_FILE is not in the expected format!"
+		else
+			sudo sed -i .bak -e \
+				"s/# $TOUCHID_LINE/$TOUCHID_LINE/" \
+				"$PAM_LOCAL_FILE"
+			sudo rm "$PAM_LOCAL_FILE.bak"
+			echo "TouchID sudo configuration complete."
+		fi
 	fi
 fi
