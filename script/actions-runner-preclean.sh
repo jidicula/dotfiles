16ba48baafd95432355815dc2723c9997a6de8c3
jidicula <git@johanan.dev>
2023-12-17 21:16:10 -0500

    ci(macos): Remove docker and kubectl from cleanup script

--- script/actions-runner-preclean.sh
+++ script/actions-runner-preclean.sh
@@ -7,7 +7,7 @@ if [[ $OSTYPE == darwin* ]]; then
 	rm /usr/local/bin/aws* \
 		/usr/local/bin/go* \
 		/usr/local/bin/dotnet \
-		/usr/local/bin/docker-compose \
-		/usr/local/bin/kubectl \
-		/usr/local/bin/2to3
+		\
+		/usr/local/bin/2to3 # /usr/local/bin/docker-compose \
+	# /usr/local/bin/kubectl \
 fi
