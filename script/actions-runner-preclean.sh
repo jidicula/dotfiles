56eeb551109d087312ae4a6fa8b771666208b2d4
jidicula <git@johanan.dev>
2024-02-04 12:13:48 -0500

    ci(macos): Remove go and 2to3 removal from cleanup script

--- script/actions-runner-preclean.sh
+++ script/actions-runner-preclean.sh
@@ -5,9 +5,5 @@ if [[ $OSTYPE == darwin* ]]; then
 	sudo rm -rf /Applications/!(Safari.app|Xcode*)
 	shopt -u extglob
 	rm /usr/local/bin/aws* \
-		/usr/local/bin/go* \
-		/usr/local/bin/dotnet \
-		\
-		/usr/local/bin/2to3 # /usr/local/bin/docker-compose \
-	# /usr/local/bin/kubectl \
+		/usr/local/bin/dotnet
 fi
