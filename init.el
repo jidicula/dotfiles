9cc2af8bff7dac6d658611159a04445ceb551a57
jidicula <git@johanan.dev>
2024-02-05 19:39:12 -0500

    fix(emacs): Set longer post-commit hook timeout for Magit

--- init.el
+++ init.el
@@ -213,6 +213,7 @@ There are two things you can do about this warning:
 	#'magit-display-buffer-fullframe-status-v1)
   ;; magit transient levels, allows GPG option to be visible
   (setq transient-default-level 5)
+  (setq git-commit-post-finish-hook-timeout 2)
   ;; Refresh magit buffer on save. This must only be evaluated when in
   ;; magit-mode. DO NOT refactor with use-package `:hook` directive.
   (with-eval-after-load 'magit-mode
